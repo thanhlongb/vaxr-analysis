80a927d1b25978e4d1321061bad53140f5611a6d
thanhlongb <thlong.hcm@gmail.com>
2023-08-16 22:20:20 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="bg-gray-200 flex items-center justify-center h-screen">
       <div className='max-w-4xl grow mx-12'>
-        <h1 className='text-2xl font-bold text-gray-900 mb-4'>Analysis:</h1>
+        <h1 className='text-2xl font-bold text-gray-900 mb-4'>Analysis</h1>
         <List />
       </div>
     </div>
